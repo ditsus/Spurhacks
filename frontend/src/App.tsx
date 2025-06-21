530c51322046321aea8b3c47191171f9349e6364
Victor Su <susuvictorvictor@gmail.com>
2025-06-21 15:26:22 -0400

    Fixed routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/login"   element={<Login />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
