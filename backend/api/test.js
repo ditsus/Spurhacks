825f33d114667a5f8008a1d1a879e2327ca2247e
Victor Su <susuvictorvictor@gmail.com>
2025-06-21 18:38:32 -0400

    Changed file structure for API

---
+++ backend/api/test.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+const router = Router();
+
+// GET /api/test
+router.get('/', (req, res) => {
+  res.json({ message: 'Test endpoint OK' });
+});
+
+export default router;
